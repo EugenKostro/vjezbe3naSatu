28faad13cf03e31844ceb3b07b0191bc9acb4bf6
Eugen101 <55134543+EugenKostro@users.noreply.github.com>
2020-02-06 10:21:14 +0100

    Add files via upload

---
+++ kostroEugenConsoleApp/kostroEugenConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kostroEugenConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+
+            float x;
+
+            Console.WriteLine("a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("c: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            x = (a + b + c) / 3;
+            Console.WriteLine("x= "+x);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
